d5abdcad4e60b1134c7dc43f809be1f86554eb6e
Zaluskyy <70617924+Zaluskyy@users.noreply.github.com>
2024-10-23 02:48:54 +0200

    add auto login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,34 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Login from "./components/Login";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import MessageContext from "./context/context";
 import Profile from "./components/Profile";
 
 export default function Home() {
   const messageContext = useContext(MessageContext);
-  const { logged } = messageContext;
+  const { logged, setLogged, setUserId, setUserName } = messageContext;
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await fetch("http://localhost:5093/check-auth", {
+          method: "GET",
+          credentials: "include",
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setLogged(data.isAuthenticated);
+          setUserId(data.id);
+          setUserName(data.name);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    checkAuth();
+  }, []);
 
   return (
     <div className={styles.Home}>
